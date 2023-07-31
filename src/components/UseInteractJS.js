73c1e3928719f50a5feed394399ddf72b538b635
mrlangloi <rcheung1997@gmail.com>
2023-07-31 03:50:26 -0700

    modified document event into element event

--- src/components/UseInteractJS.js
+++ src/components/UseInteractJS.js
@@ -5,14 +5,12 @@ import { WebSocketContext } from './WebSocketContext';
 
 function UseInteractJS(imageProperties) {
   const elementRef = useRef(null);
-  const lastClickedID = useRef(null);
-  
-  const socket = useContext(WebSocketContext);
+
+  const { socket, lastClickedID } = useContext(WebSocketContext);
 
   // Update the image properties in the database
   const handleImageUpdate = async () => {
-    const response = await axios.put(`http://localhost:8080/images/${imageProperties.current.imageID}`, imageProperties.current);
-    console.log(response.data)
+    await axios.put(`http://localhost:8080/images/${imageProperties.current.imageID}`, imageProperties.current);
   };
 
   // Handles the draggable and resizable behavior of the image
@@ -21,72 +19,79 @@ function UseInteractJS(imageProperties) {
     const element = elementRef.current;
 
     const interactInstance = interact(element)
-    .draggable({
-      // Customize the draggable behavior here if needed
-      onmove: async (event) => {
-        const target = event.target;
+      .draggable({
+        // Customize the draggable behavior here if needed
+        onmove: async (event) => {
 
-        // Update the position data
-        imageProperties.current.x += event.dx;
-        imageProperties.current.y += event.dy;
-
-        // Translate the element's position
-        target.style.transform = `translate(${imageProperties.current.x}px, ${imageProperties.current.y}px) rotate(${imageProperties.current.rotation}rad)`;
-
-        // Emit the updated image position and size to the server
-        socket.emit('updateImage', imageProperties.current);
-      },
-      modifiers: [
-        // Enable minimum and maximum constraints
-        interact.modifiers.restrictRect({
-          restriction: 'parent',
-          endOnly: true,
-        }),
-      ],
-      onend: handleImageUpdate,
-      
-    }).resizable({
-      // resize from all edges and corners
-      edges: { left: true, right: true, bottom: true, top: true },
-  
-      listeners: {
-        async move (event) {
           const target = event.target;
 
-          // update the element's style
-          imageProperties.current.width = target.style.width = event.rect.width;
-          imageProperties.current.height = target.style.height = event.rect.height;
-  
-          // translate when resizing from top or left edges
-          imageProperties.current.x += event.deltaRect.left;
-          imageProperties.current.y += event.deltaRect.top;
-  
+          // Update the position data
+          imageProperties.current.x += event.dx;
+          imageProperties.current.y += event.dy;
+
+          // Translate the element's position
           target.style.transform = `translate(${imageProperties.current.x}px, ${imageProperties.current.y}px) rotate(${imageProperties.current.rotation}rad)`;
 
           // Emit the updated image position and size to the server
           socket.emit('updateImage', imageProperties.current);
-        }
-      },
-      modifiers: [
-        // keep the edges inside the parent
-        interact.modifiers.restrictEdges({
-          outer: 'parent'
-        }),
-  
-        // minimum size
-        interact.modifiers.restrictSize({
-          min: { width: 50, height: 50 }
-        })
-      ],
-      onend: handleImageUpdate,
+        },
+        modifiers: [
+          // Enable minimum and maximum constraints
+          interact.modifiers.restrictRect({
+            restriction: 'parent',
+            endOnly: true,
+          }),
+        ],
+        onend: handleImageUpdate,
 
-    }).on('mousedown', (event) => {
-      lastClickedID.current = imageProperties.current.imageID;
-      console.log(`Last clicked image ID: ${lastClickedID.current}`);
-    });
+      }).resizable({
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: true, top: true },
 
-    // Listen for arrow key presses to change the image's z-index
-    document.addEventListener('keydown', async (event) => {
+        listeners: {
+          async move(event) {
+            const target = event.target;
+
+            // update the element's style
+            imageProperties.current.width = target.style.width = event.rect.width;
+            imageProperties.current.height = target.style.height = event.rect.height;
+
+            // translate when resizing from top or left edges
+            imageProperties.current.x += event.deltaRect.left;
+            imageProperties.current.y += event.deltaRect.top;
+
+            target.style.transform = `translate(${imageProperties.current.x}px, ${imageProperties.current.y}px) rotate(${imageProperties.current.rotation}rad)`;
+
+            // Emit the updated image position and size to the server
+            socket.emit('updateImage', imageProperties.current);
+          }
+        },
+        modifiers: [
+          // keep the edges inside the parent
+          interact.modifiers.restrictEdges({
+            outer: 'parent'
+          }),
+
+          // minimum size
+          interact.modifiers.restrictSize({
+            min: { width: 50, height: 50 }
+          })
+        ],
+        onend: handleImageUpdate,
+
+      });
+    element.addEventListener('mouseup', (event) => {
+      event.stopPropagation();
+      if (event.currentTarget === element) {
+        lastClickedID.current = imageProperties.current.imageID;
+        console.log(`Last clicked image ID: ${lastClickedID.current}`);
+      }
+    })
+
+    // // Listen for arrow key presses to change the image's z-index
+    document.addEventListener('keydown', (event) => {
+      event.stopPropagation();
+
       const changeZIndex = () => {
         socket.emit('updateImage', imageProperties.current);
         handleImageUpdate();
@@ -96,20 +101,22 @@ function UseInteractJS(imageProperties) {
         await axios.delete(`http://localhost:8080/images/${lastClickedID.current}`);
         socket.emit('deleteImage', lastClickedID.current);
       };
-      
-      if(event.key === 'Backspace' && lastClickedID.current) {
+
+      if (event.key === 'Backspace' && lastClickedID.current === imageProperties.current.imageID) {
         event.preventDefault(); // Prevent the browser from going back to the previous page
         setTimeout(removeImage, 250);
       }
-      else if (event.key === 'ArrowUp') {
+      else if (event.key === 'ArrowUp' && lastClickedID.current === imageProperties.current.imageID) {
         event.preventDefault(); // Prevent the browser from scrolling up
         imageProperties.current.zIndex += 1;
         setTimeout(changeZIndex, 100);
+        console.log('Current z-index: ' + imageProperties.current.zIndex);
       }
-      else if (event.key === 'ArrowDown') {
+      else if (event.key === 'ArrowDown' && lastClickedID.current === imageProperties.current.imageID) {
         event.preventDefault(); // Prevent the browser from scrolling down
         imageProperties.current.zIndex -= (imageProperties.current.zIndex === 2 ? 0 : 1);
         setTimeout(changeZIndex, 100);
+        console.log('Current z-index: ' + imageProperties.current.zIndex);
       }
     });
 
@@ -138,10 +145,10 @@ function UseInteractJS(imageProperties) {
     // Clean up the interact instance and event listener when the component unmounts
     return () => {
       interactInstance.unset();
-      document.removeEventListener('keydown', () => {});
+      element.removeEventListener('click', () => { });
+      document.removeEventListener('keydown', () => { });
     };
-  }, [socket]);
-
+  }, []);
 
 
   return elementRef;
