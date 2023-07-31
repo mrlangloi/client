d7ebba05b79de5622c46ab52228619567afa0b10
mrlangloi <rcheung1997@gmail.com>
2023-07-30 23:26:38 -0700

    refactored to use a useRef that stores values

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import UseInteractJS from "./UseInteractJS";
-import UseRotate from "./UseRotate";
+// import UseRotate from "./UseRotate";
 
 function Images(props) {
 
-  const { image, id, index }  = props;
+  const { imageData, x, y, width, height, rotation, zIndex, opacity }  = props;
 
-  const elementRef = UseInteractJS(id);
-  const rotationRef = UseRotate(id);
+  const imageProperties = useRef({
+    imageID: props.imageID,
+    x: x,
+    y: y,
+    width: width,
+    height: height,
+    rotation: rotation,
+    zIndex: zIndex,
+    opacity: opacity,
+  });
+
+  const elementRef = UseInteractJS(imageProperties);
+  // const rotationRef = UseRotate(imageProperties);
 
   return (
     <div 
       className='imgDiv resize-drag resize-border' 
       ref={elementRef}
       style={{
-        transform: 'translate(0px, 0px) rotate(0rad)',
-        width: '250px',
-        height: '250px',
-        opacity: '1',
-        zIndex: {index},
+        transform: `translate(${imageProperties.current.x}px, ${imageProperties.current.y}px) rotate(${imageProperties.current.rotation}rad)`,
+        width: `${imageProperties.current.width}px`,
+        height: `${imageProperties.current.height}px`,
+        zIndex: `${imageProperties.current.zIndex}`,
+        opacity: `${imageProperties.current.opacity}`,
       }}      
     >
-      <img className='interactive-img' alt='' src={image} height='250' />
-      <div className='rotation-handle' ref={rotationRef}>A</div>
+      <img className='interactive-img' alt='' src={imageData} height='250' />
+      {/* <div className='rotation-handle' ref={rotationRef}>A</div> */}
     </div>
   )
 }
