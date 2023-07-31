6a6d39b71a4318d3ab79d18485d5026fc835f920
mrlangloi <rcheung1997@gmail.com>
2023-07-30 23:28:03 -0700

    refactored function to use the useRef passed in

--- src/components/UseInteractJS.js
+++ src/components/UseInteractJS.js
@@ -3,12 +3,17 @@ import interact from 'interactjs';
 import { useContext, useEffect, useRef } from 'react';
 import { WebSocketContext } from './WebSocketContext';
 
-function UseInteractJS(id) {
+function UseInteractJS(imageProperties) {
   const elementRef = useRef(null);
-  const imageID = useRef(id);
   const lastClickedID = useRef(null);
-
+  
   const socket = useContext(WebSocketContext);
+
+  // Update the image properties in the database
+  const handleImageUpdate = async () => {
+    const response = await axios.put(`http://localhost:8080/images/${imageProperties.current.imageID}`, imageProperties.current);
+    console.log(response.data)
+  };
 
   // Handles the draggable and resizable behavior of the image
   useEffect(() => {
@@ -20,28 +25,16 @@ function UseInteractJS(id) {
       // Customize the draggable behavior here if needed
       onmove: async (event) => {
         const target = event.target;
-        const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-        const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-        const rotation = parseFloat(target.getAttribute('data-angle')) || 0;
+        // Update the position data
+        imageProperties.current.x += event.dx;
+        imageProperties.current.y += event.dy;
 
         // Translate the element's position
-        target.style.transform = `translate(${x}px, ${y}px) rotate(${rotation}rad)`;
+        target.style.transform = `translate(${imageProperties.current.x}px, ${imageProperties.current.y}px) rotate(${imageProperties.current.rotation}rad)`;
 
-        // Store the updated position data for the next move event
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-        
         // Emit the updated image position and size to the server
-        socket.emit('updateImage', {
-          key: imageID.current,
-          x,
-          y,
-          width: target.style.width,
-          height: target.style.height,
-          rotation: parseFloat(target.getAttribute('data-angle')) || 0,
-          zIndex: target.style.zIndex,
-        });
+        socket.emit('updateImage', imageProperties.current);
       },
       modifiers: [
         // Enable minimum and maximum constraints
@@ -50,6 +43,8 @@ function UseInteractJS(id) {
           endOnly: true,
         }),
       ],
+      onend: handleImageUpdate,
+      
     }).resizable({
       // resize from all edges and corners
       edges: { left: true, right: true, bottom: true, top: true },
@@ -57,34 +52,19 @@ function UseInteractJS(id) {
       listeners: {
         async move (event) {
           const target = event.target;
-          var x = (parseFloat(target.getAttribute('data-x')) || 0);
-          var y = (parseFloat(target.getAttribute('data-y')) || 0);
-  
+
           // update the element's style
-          target.style.width = event.rect.width + 'px';
-          target.style.height = event.rect.height + 'px';
+          imageProperties.current.width = target.style.width = event.rect.width;
+          imageProperties.current.height = target.style.height = event.rect.height;
   
           // translate when resizing from top or left edges
-          x += event.deltaRect.left;
-          y += event.deltaRect.top;
-
-          const rotation = parseFloat(target.getAttribute('data-angle')) || 0;
+          imageProperties.current.x += event.deltaRect.left;
+          imageProperties.current.y += event.deltaRect.top;
   
-          target.style.transform = `translate(${x}px, ${y}px) rotate(${rotation}rad)`;
-  
-          target.setAttribute('data-x', x);
-          target.setAttribute('data-y', y);
+          target.style.transform = `translate(${imageProperties.current.x}px, ${imageProperties.current.y}px) rotate(${imageProperties.current.rotation}rad)`;
 
           // Emit the updated image position and size to the server
-          socket.emit('updateImage', {
-            key: imageID.current,
-            x,
-            y,
-            width: target.style.width,
-            height: target.style.height,
-            rotation: parseFloat(target.getAttribute('data-angle')) || 0,
-            zIndex: target.style.zIndex,
-          });
+          socket.emit('updateImage', imageProperties.current);
         }
       },
       modifiers: [
@@ -98,59 +78,60 @@ function UseInteractJS(id) {
           min: { width: 50, height: 50 }
         })
       ],
+      onend: handleImageUpdate,
 
     }).on('mousedown', (event) => {
-      lastClickedID.current = imageID.current;
-      // console.log(`Last clicked image ID: ${lastClickedID.current}`);
+      lastClickedID.current = imageProperties.current.imageID;
+      console.log(`Last clicked image ID: ${lastClickedID.current}`);
     });
 
     // Listen for arrow key presses to change the image's z-index
     document.addEventListener('keydown', async (event) => {
-      const target = event.target;
-      const zIndex = target.style.zIndex;
-
-      const changeZIndex = (newZIndex) => {
-        socket.emit('updateImage', {
-          key: imageID.current,
-          x: target.getAttribute('data-x'),
-          y: target.getAttribute('data-y'),
-          width: target.style.width,
-          height: target.style.height,
-          zIndex: newZIndex,
-        });
+      const changeZIndex = () => {
+        socket.emit('updateImage', imageProperties.current);
+        handleImageUpdate();
       };
 
-      // event.preventDefault(); prevents the page from scrolling up/down when the arrow keys are pressed
-      if (event.key === 'ArrowUp') {
-        event.preventDefault();
-        changeZIndex(parseInt(zIndex) + 1);
-      }
-      else if (event.key === 'ArrowDown') {
-        event.preventDefault();
-        changeZIndex(parseInt(zIndex) - 1);
-      }
-    });
-
-    // Delete the image when the backspace key is pressed
-    document.addEventListener('keydown', async (event) => {
       const removeImage = async () => {
         await axios.delete(`http://localhost:8080/images/${lastClickedID.current}`);
+        socket.emit('deleteImage', lastClickedID.current);
       };
-      if(event.key === 'Backspace' && lastClickedID) {
+      
+      if(event.key === 'Backspace' && lastClickedID.current) {
+        event.preventDefault(); // Prevent the browser from going back to the previous page
         setTimeout(removeImage, 250);
       }
+      else if (event.key === 'ArrowUp') {
+        event.preventDefault(); // Prevent the browser from scrolling up
+        imageProperties.current.zIndex += 1;
+        setTimeout(changeZIndex, 100);
+      }
+      else if (event.key === 'ArrowDown') {
+        event.preventDefault(); // Prevent the browser from scrolling down
+        imageProperties.current.zIndex -= (imageProperties.current.zIndex === 2 ? 0 : 1);
+        setTimeout(changeZIndex, 100);
+      }
     });
-
 
     // Listen for updates from the server for all images
     socket.on('updatedImage', (data) => {
       // Update the image position, size, and rotation based on the received data
       const target = elementRef.current;
-      if (data.key === imageID.current) {
+      if (data.imageID === imageProperties.current.imageID) {
         target.style.transform = `translate(${data.x}px, ${data.y}px) rotate(${data.rotation}rad)`;
-        target.style.width = data.width;
-        target.style.height = data.height;
+        target.style.width = data.width + 'px';
+        target.style.height = data.height + 'px';
         target.style.zIndex = data.zIndex;
+        target.style.opacity = data.opacity;
+
+        // Update the image data
+        imageProperties.current.x = data.x;
+        imageProperties.current.y = data.y;
+        imageProperties.current.rotation = data.rotation;
+        imageProperties.current.width = data.width;
+        imageProperties.current.height = data.height;
+        imageProperties.current.zIndex = data.zIndex;
+        imageProperties.current.opacity = data.opacity;
       }
     });
 
