dc0fdb5d9c48fa89244c4639cc964a33b576b507
mrlangloi <rcheung1997@gmail.com>
2023-07-31 21:49:00 -0700

    added quotes to src

--- src/components/TwitchStream.js
+++ src/components/TwitchStream.js
@@ -3,7 +3,7 @@ import React from "react";
 function TwitchStream() {
   return (
     <div className='twitchStreamDiv'>
-          <iframe className='twitchStream' src={`https://player.twitch.tv/?channel=dearbun&parent=${process.env.DOMAIN_NAME}`} allowFullScreen={false} width="1920" height="1080"></iframe>
+          <iframe className='twitchStream' src={`"https://player.twitch.tv/?channel=dearbun&parent=${process.env.DOMAIN_NAME}"`} allowFullScreen={false} width="1920" height="1080"></iframe>
     </div>
   )
 }
